920703e008e88319445ae369bf7382382e32fcb9
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2025-04-27 14:47:16 +0100

    # update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'DHCP - Digital Help Connected Platforms',
+  description: 'Created with <3 byt DHCP Team',
 }
 
 export default function RootLayout({
