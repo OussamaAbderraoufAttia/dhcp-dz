d59481ed779851bd24b3a69bbc50423476864455
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2025-05-05 13:35:37 +0100

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
-// Remove 'use client' from here
 import type { Metadata } from 'next'
 import './globals.css'
 
+// ✅ DO NOT import favicon like a module
+// import favicon from "/app/favicon.ico" ← Remove this
+
 export const metadata: Metadata = {
   title: 'DHCP - Digital Help Connected Platforms',
-  description: 'Created with <3 byt DHCP Team',
+  description: 'Created with <3 by DHCP Team',
+  icons: {
+    icon: [
+      {url: '/favicon.ico',} // ✅ Path relative to public/
+    ]
+  },
 }
 
 export default function RootLayout({
